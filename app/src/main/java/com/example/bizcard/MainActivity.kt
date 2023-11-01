6345a35b96038fbfc53c452c884eba8749da1432
reisalazar <01110709salazarbaldelli>
2023-11-01 14:13:05 -0300

    Adding profile image

--- app/src/main/java/com/example/bizcard/MainActivity.kt
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bizcard.ui.theme.BizcardTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +30,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                )
+                {
+                    CreateBizCard()
                 }
             }
         }
@@ -30,14 +40,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateBizCard() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+            elevation = 4.dp
+        ) {
+            Surface(
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(
+                    0.5.dp, Color.LightGray
+                ),
+                elevation = 4.dp,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.perfil_anonimo),
+                    contentDescription = "profile image",
+                    modifier = Modifier.size(135.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BizcardTheme {
-        Greeting("Android")
+        CreateBizCard()
     }
 }
